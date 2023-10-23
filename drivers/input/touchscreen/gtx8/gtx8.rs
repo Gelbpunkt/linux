12c1827b05fc4541883172883370d5afcf0ca2c4
Jens Reidel <adrian@travitia.xyz>
2023-10-23 03:48:04 +0200

    WIP

---
+++ drivers/input/touchscreen/gtx8/gtx8.rs
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0
+
+//! Rust minimal sample.
+
+use kernel::prelude::*;
+
+module! {
+    type: Gtx8,
+    name: "gtx8rs",
+    author: "Jens Reidel <adrian@travitia.xyz>",
+    description: "Driver for GTx8 touchscreens",
+    license: "GPL",
+}
+
+struct Gtx8 {
+    numbers: Vec<i32>,
+}
+
+impl kernel::Module for RustMinimal {
+    fn init(_module: &'static ThisModule) -> Result<Self> {
+        pr_info!("Rust minimal sample (init)\n");
+        pr_info!("Am I built-in? {}\n", !cfg!(MODULE));
+
+        let mut numbers = Vec::new();
+        numbers.try_push(72)?;
+        numbers.try_push(108)?;
+        numbers.try_push(200)?;
+
+        Ok(RustMinimal { numbers })
+    }
+}
+
+impl Drop for RustMinimal {
+    fn drop(&mut self) {
+        pr_info!("My numbers are {:?}\n", self.numbers);
+        pr_info!("Rust minimal sample (exit)\n");
+    }
+}
+
